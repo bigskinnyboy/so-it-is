c500c0b0a4cd18803166f2ea6ec92b86fab9991a
Aaron Witherow <aaron@alliancesoftware.com.au>
2014-10-17 14:13:16 +1100

    Tidying up broken code and adding commonly used features like bootstrap forms and tabs

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,72 @@
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    clean = require('gulp-clean'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload');
+		sass = require('gulp-ruby-sass'),
+		autoprefix = require('gulp-autoprefixer'),
+		rename = require('gulp-rename'),
+		concat = require('gulp-concat'),
+		minifyCSS = require('gulp-minify-css'),
+		jshint = require('gulp-jshint'),
+		uglify = require('gulp-uglify'),
+		stylish = require('jshint-stylish');
 
-    gulp.task('styles', function() {
-      return gulp.src('sass/**/*.scss')
-        .pipe(sass({
-          require: ['susy'],
-          //compass: true,
-          style: 'expanded'
-          }))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest('css'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifycss())
-        .pipe(gulp.dest('css'))
-        .pipe(notify({ message: 'Styles task complete' }));
-    });
+var paths = {
+	scripts: [
+		'js/plugins/*.js',
+		'js/_*.js'
+	],
+	jshint: [
+		'gulpfile.js',
+		'js/*.js',
+		'!js/scripts.js',
+		'!js/scripts.min.js',
+		'!**/*.min-*'
+	],
+	sass: [
+		'sass/styles.scss',
+		'sass/**/*.scss',
+		'sass/styles-old-ie.scss'
+	]
+};
 
-    gulp.task('scripts', function() {
-      return gulp.src('js/**/*.js')
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('default'))
-        .pipe(concat('_main.js'))
-        .pipe(gulp.dest('js'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest('js'))
-        .pipe(notify({ message: 'Scripts task complete' }));
-    });
+var destination = {
+	css: 'css',
+	scripts: 'js',
+	vendor: 'js/vendor'
+};
 
-    gulp.task('images', function() {
-      return gulp.src('img/**/*')
-        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-        .pipe(gulp.dest('img'))
-        .pipe(notify({ message: 'Images task complete' }));
-    });
+gulp.task('sass', function() {
+	return gulp.src(paths.sass)
+			.pipe(sass({compass: true, require: 'susy', 'sourcemap=none': true})).on('error', function(err) {
+				console.warn(err.message);
+			})
+			.pipe(autoprefix('last 2 versions', 'ie 8', 'ie 9', 'android 2.3', 'android 4', 'opera 12'))
+			.pipe(gulp.dest(destination.css))
+			.pipe(minifyCSS({noAdvanced: true}))
+			.pipe(rename({suffix: ".min"}))
+			.pipe(gulp.dest(destination.css));
+});
 
-    gulp.task('clean', function() {
-      return gulp.src(['css', 'js', 'img'], {read: false})
-        .pipe(clean());
-    });
+gulp.task('jshint', function() {
+	return gulp.src(paths.jshint)
+			.pipe(jshint())
+			.pipe(jshint.reporter(stylish));
+});
 
-    gulp.task('default', ['clean'], function() {
-      gulp.start('styles', 'scripts', 'images');
-    });
+gulp.task('js', ['jshint'], function() {
+	return gulp.src(paths.scripts)
+			.pipe(concat('./scripts.js'))
+			.pipe(gulp.dest(destination.scripts))
+			.pipe(uglify())
+			.pipe(rename('./scripts.min.js'))
+			.pipe(gulp.dest(destination.scripts))
+});
 
-    gulp.task('watch', function() {
 
-      // Watch .scss files
-      gulp.watch('sass/**/*.scss', ['styles']);
+gulp.task('watch', function() {
+	gulp.watch('sass/**/*.scss', ['sass']);
+	gulp.watch('js/**/*.js', ['jshint', 'js']);
+});
 
-      // Watch .js files
-      gulp.watch('js/**/*.js', ['scripts']);
+gulp.task('default', ['sass', 'jshint', 'js']);
+gulp.task('dev', ['default']);
+gulp.task('build', ['sass', 'jshint', 'js']);
 
-      // Watch image files
-      gulp.watch('img/**/*', ['images']);
-
-    });
